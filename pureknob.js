079a9b638c0214baadc4e729765c83e412bd3711
Andre Plötze <7642349+andrepxx@users.noreply.github.com>
2020-05-29 18:08:19 +0200

    Fix whitespace

--- pureknob.js
+++ pureknob.js
@@ -21,29 +21,33 @@
 
 "use strict";
 
- (function (root, factory) {
-   if (typeof define === 'function' && define.amd) {
-     // AMD. Register as an anonymous module.
-       define([], factory);
-   } else if (typeof module === 'object' && module.exports) {
-     // Node. Does not work with strict CommonJS, but
-     // only CommonJS-like environments that support module.exports,
-     // like Node.
-     module.exports = factory();
-   } else {
-     // Browser globals (root is window)
-     root.pureknob = factory();
-   }
- }(typeof self !== 'undefined' ? self : this, function () {
-   // Just return a value to define the module export.
-   // This example returns an object, but the module
-   // can return a function as the exported value.
-   function PureKnob() {
+(function (root, factory) {
+
+	if ((typeof define === 'function') && define.amd) {
+		/* AMD. Register as an anonymous module. */
+		define([], factory);
+	} else if ((typeof module === 'object') && module.exports) {
+		/*
+		 * Node.
+		 *
+		 * Does not work with strict CommonJS, but
+		 * only CommonJS-like environments that support
+		 * module.exports, like Node.
+		 */
+		module.exports = factory();
+	} else {
+		/* Browser globals (root is window). */
+		root.pureknob = factory();
+	}
+
+} (typeof self !== 'undefined' ? self : this, function () {
+
+	function PureKnob() {
 
 		/*
 		 * Creates a bar graph element.
 		 */
-	   	this.createBarGraph = function(width, height) {
+		this.createBarGraph = function(width, height) {
 			const heightString = height.toString();
 			const widthString = width.toString();
 			const canvas = document.createElement('canvas');
@@ -1131,9 +1135,8 @@
 			return knob;
 		};
 
-   }
+	}
 
-   return new PureKnob();
-
- }));
+	return new PureKnob();
+}));
 
